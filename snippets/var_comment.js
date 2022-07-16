84ac03b6a7ab2fca991833d81abefb5405bd1137
tikkhun <s565338107@163.com>
2022-07-16 16:37:54 +0800

    feat: 暂时没有生成可用插件。。。

---
+++ snippets/var_comment.js
@@ -0,0 +1,16 @@
+const body = `
+\/**
+ * @var $1  
+ * @desc $变量定义
+ * @property {string} $2 $变量名
+ * 
+ */
+`;
+const description = "";
+
+module.exports = {
+  prefix: ["!vh"],
+  type: ["javascript", "typescript"],
+  description: description || body,
+  body: body.trim(),
+};
