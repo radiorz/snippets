b803623168240667cc323a735a17443a20c196a4
tikkhun <s565338107@163.com>
2022-07-16 15:11:25 +0800

    fix：different user

--- src/publish.ps1
+++ src/publish.ps1
@@ -1,4 +1,3 @@
 $Source= "./build/*"
-$Dest = "C:\Users\tikkhun\AppData\Roaming\Code\User\snippets"
-
+$Dest = "$env:APPDATA\Roaming\Code\User\snippets"
 Copy-Item -Path $Source -Destination $Dest -Force -Recurse -ErrorAction Stop 
