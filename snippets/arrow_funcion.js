c683ef21d3050e3e23f18429d85b6ed42bfa0387
tikkhun <s565338107@163.com>
2022-08-16 17:08:23 +0800

    chore: 箭头函数错误

--- snippets/arrow_funcion.js
+++ snippets/arrow_funcion.js
@@ -2,7 +2,6 @@
 const body = `($1) => { $2 }`;
 const description = "";
 module.exports = {
-  key: "array_func",
   prefix: ["anfn"],
   type: ["javascript", "typescript",'vue'],
   description: description || body,
