8e83528de711e7bf816a4bd8e49bfd779dbaea3f
zksu <565338107@qq.com>
2022-11-16 15:40:22 +0800

    feat: memo 还是不立即用比较好点..

--- snippets/rn_func_component.js
+++ snippets/rn_func_component.js
@@ -1,18 +1,18 @@
 const { origin: fileHeader } = require("./file_header");
 const { origin: fileName } = require("./file_name");
 const body = `${fileHeader}
-import { View, Text } from 'react-native'
 import React, { useState, useEffect, memo } from 'react'
+import { View, Text } from 'react-native'
 
- function \${1:${fileName}}() {
+function \${1:${fileName}}() {
   return (
     <View>
       <Text>\${1:${fileName}}</Text>
     </View>
   )
 }
-export default memo(\${1:${fileName}})
 
+export default \${1:${fileName}}
 
 `;
 
