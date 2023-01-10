cf35e39216d7ed05eb8eee6eb0d178fe2bcf3f30
zksu <565338107@qq.com>
2023-01-10 09:25:39 +0800

    feat: afn

--- test/Component/VueComponent.vue
+++ test/Component/VueComponent.vue
@@ -17,19 +17,28 @@
 -->
 
 <script>
-  export default {
-    name: ""
-  };
-</script>
-<script setup>
-
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'VueComponent',
+  // components: {},
+  props: {},
+  // 从组件触发 emits 事件
+  // emits:{},
+  // 暴露
+  // expose: {},
+  data() {
+    return {}
+  }
+  // computed:{},
+  // watch:{},
+  // create(){},
+  // methods:{}
+})
 </script>
 
 <template>
-  <div class="">
+  <div class="VueComponent">
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>"
+<style lang="scss" scoped></style>"
