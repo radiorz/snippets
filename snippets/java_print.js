9075d3c6d3a8549b8dc0379746ae19058660782f
zksu <565338107@qq.com>
2023-06-13 11:04:21 +0800

    feat: java print not sout

---
+++ snippets/java_print.js
@@ -0,0 +1,9 @@
+const body = `System.out.println("$1"+$1);`;
+const description = "";
+
+module.exports = {
+  prefix: ["print"],
+  type: ["java"],
+  description: description || body,
+  body: body.trim(),
+};
