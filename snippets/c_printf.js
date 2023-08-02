95eab27cab873b521e22a30cf7ae751785a2d37e
tikkhun <s565338107@163.com>
2023-08-02 09:39:24 +0800

    feat: c_printf

---
+++ snippets/c_printf.js
@@ -0,0 +1,11 @@
+const body = `
+printf("$1 \${2:%d}\n",$1);
+`;
+const description = "";
+
+module.exports = {
+  prefix: ["printf"],
+  type: ["c"],
+  description: description || body,
+  body: body.trim(),
+};
