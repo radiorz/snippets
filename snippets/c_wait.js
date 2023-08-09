cdd369a9582391b1760463056fd70cb91a08a214
zksu <565338107@qq.com>
2023-08-09 11:01:46 +0800

    feat: c语言 c++的等待

---
+++ snippets/c_wait.js
@@ -0,0 +1,18 @@
+const body = `
+#include <time.h>
+
+void delay(unsigned int milliseconds) {
+    clock_t start_time = clock(); // 获取当前时钟时间
+    while (clock() < start_time + milliseconds); // 循环等待直到达到指定的时间
+    return;
+}
+`;
+const description =
+  "这个是使用原生时间循环进行的delay,大部分时候应该使用线程的sleep方法可能会常用一些";
+
+module.exports = {
+  prefix: ["!wait", "!delay"],
+  type: ["c"],
+  description: description || body,
+  body: body.trim(),
+};
--- snippets/cpp_wait.js
+++ snippets/cpp_wait.js
@@ -1,6 +1,6 @@
 const body = `
-void wait(int second = 50){
-    std::this_thread::sleep_for(std::chrono::milliseconds(second));
+void wait(int milliseconds = 50){
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
 `;
 const description = "";
