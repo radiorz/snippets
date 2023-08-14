957187252f01e95771d77bf157f5522f63d19336
tikkhun <s565338107@163.com>
2023-08-14 16:12:37 +0800

    feat: cmake set

---
+++ snippets/cmake_set.js
@@ -0,0 +1,11 @@
+const body = `
+set($1: "$2")
+`;
+const description = "";
+
+module.exports = {
+  prefix: ["set"],
+  type: ["cmake"],
+  description: description || body,
+  body: body.trim(),
+};
