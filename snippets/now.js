12e3e7828e27aa8a3e8be271541eb573405982d6
tikkhun <s565338107@163.com>
2023-09-01 17:32:14 +0800

    feat:now 添加使用场景

--- snippets/now.js
+++ snippets/now.js
@@ -3,7 +3,7 @@ const body = `"$CURRENT_YEAR年$CURRENT_MONTH月$CURRENT_DATE日 $CURRENT_HOUR:$
 module.exports = {
   prefix: ["now"],
   // 这种通用的或许全部平台都可以加一个（或许要制定一个 type：all 的命令
-  type: ["javascript", "typescript", "java", "vue"],
+  type: ["javascript", "typescript", "java", "vue",'markdown'],
   body,
   description: "当前时间",
 };
