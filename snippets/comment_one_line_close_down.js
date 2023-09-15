97b45aba25b2e15d8c5a10c1761c97b5aba31913
zksu <565338107@qq.com>
2023-09-15 08:40:22 +0800

    feat: comment

---
+++ snippets/comment_one_line_close_down.js
@@ -0,0 +1,21 @@
+/**
+ * @author
+ * @file
+ * @desc 单行封闭注释
+ * @todo
+ *
+ * @done
+ * @example
+ */
+const { LANGUAGE_LABELS } = require("../src/common/consts/types");
+
+const body = `
+\${BLOCK_COMMENT_START} ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ \$1 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ \${BLOCK_COMMENT_END}
+`;
+const description = "/** ******  ****** */";
+module.exports = {
+  prefix: ['!down'],
+  type: LANGUAGE_LABELS,
+  description: description || body,
+  body: body.trim(),
+};
