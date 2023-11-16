344f22ce369c6d698724f0624866fdea91dbd53c
tikkhun <s565338107@163.com>
2023-11-16 20:58:38 +0800

    feat: getArrayKeyMap

--- snippetTexts/getArrayKeyMap.js
+++ snippetTexts/getArrayKeyMap.js
@@ -1,15 +1,18 @@
 /**
  * 将对象数组转化为以某个值为key的对象
- * 基础函数
+ * 例如:
+ * 将 [{name: 'abc',value:1}, {name: 'ccc', value: 2}]
+ * 转化为 {abc: {value:1},ccc: {value:2}}
  * @param {Array<Object>} array 数组
  * @param {string} key key值
  * @returns
  */
-export function getArrayKeyMap(array = [], key = "name") {
-  const pinNameMap = {};
-  array.forEach((item = {}) => {
-    if (!item[key]) return;
-    pinNameMap[item[key]] = item;
-  });
-  return pinNameMap;
+function getArrayKeyMap(array = [], key = 'name') {
+  return array.reduce((previousValue, current) => {
+    if (typeof current[key] === 'undefined') {
+      return previousValue
+    }
+    previousValue[current[key]] = current
+    return previousValue
+  }, {})
 }
