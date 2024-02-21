928beddf601c363af3909ba46dd6d6c3627e984f
tikkhun <s565338107@163.com>
2024-02-21 10:04:16 +0800

    feat: typescript sleep

--- snippets/sleep.js
+++ snippets/sleep.js
@@ -1,3 +1,4 @@
+const { TYPES } = require("../src/common/consts/types");
 const body = `
 const timeout = \${1:1000}
 await new Promise((resolve) => setTimeout(resolve, timeout))
@@ -5,8 +6,8 @@ await new Promise((resolve) => setTimeout(resolve, timeout))
 const description = "";
 
 module.exports = {
-  prefix: ["sleep","!sleep", "delay",'await'],
-  type: ["javascript"],
+  prefix: ["sleep", "!sleep", "delay", "await"],
+  type: [TYPES.javascript, TYPES.typescript],
   description: description || body,
   body: body.trim(),
 };
