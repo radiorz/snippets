9eafd1157f6baed440961e6e1dad24b5b9b8c89b
tikkhun <s565338107@163.com>
2024-05-05 08:50:21 +0800

    chore: vue3 listener

---
+++ snippets/vue3_listener.js
@@ -0,0 +1,17 @@
+const { TYPES } = require("../src/common/consts/types");
+const body = `
+function on$1(event) {
+
+}
+
+onMounted(() => window.addEventListener('$1', on$1))
+onUnmounted(() => window.removeEventListener('$1',  on$1))
+`;
+const description = "";
+
+module.exports = {
+  prefix: ["!vue3_listener"],
+  type: [TYPES.javascript],
+  description: description || body,
+  body: body.trim(),
+};
