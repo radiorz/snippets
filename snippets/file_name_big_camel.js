29798ad04a0272a6fad49c85e0d0f5903f76de74
tikkhun <s565338107@163.com>
2024-06-19 09:35:45 +0800

    chore: 补充说明。

--- snippets/file_name_big_camel.js
+++ snippets/file_name_big_camel.js
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/51277998/vs-code-how-to-convert-snippet-placeholder-from-camelcase-to-screaming-snake-ca
+// https://gist.github.com/cmstead/7ad64539d13dfdb5a2591bc3be3c8eeb
+// https://gist.github.com/natemoo-re/94cfba2bfb17cbeec0680c05309ea9ee
 const { TYPES } = require("../src/common/consts/types");
 const body =
   "${TM_FILENAME_BASE/([a-z]*)[-@_.*0-9]+([a-z]*)/${1:/capitalize}${2:/capitalize}/g}";
