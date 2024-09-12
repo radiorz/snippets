7563020acf812f5ddada8357c8266fc10d4c0bad
tikkhun <s565338107@163.com>
2024-09-12 16:15:53 +0800

    chore: todo

--- snippets/todo.js
+++ snippets/todo.js
@@ -1,18 +1,19 @@
 const { TYPES } = require("../src/common/consts/types");
 const { body: today } = require("./today");
 const body = `
-# TODO
-- $1
-# DONE
-## ${today}
-- 
-# FUTURE
+\${BLOCK_COMMENT_START}* 
+  * # TODO
+  * - $1
+  * # DONE
+  * ## ${today}
+  * # FUTURE
+ \${BLOCK_COMMENT_END}
 `;
 const description = "";
 
 module.exports = {
   prefix: ["!todo"],
-  type: [TYPES.markdown],
+  type: [TYPES.javascript, TYPES.typescript],
   description: description || body,
   body: body.trim(),
 };
