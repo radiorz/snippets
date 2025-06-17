fdba7a1b9001af8290bb359fc1971e33b66c8197
tikkhun <s565338107@163.com>
2025-06-17 17:52:10 +0800

    chore: update react.

--- snippets/react_func_component_ts.js
+++ snippets/react_func_component_ts.js
@@ -1,11 +1,12 @@
 const { origin: fileHeader } = require("./file_header");
 const { origin: fileName } = require("./file_name");
+const { TYPES } = require("../src/common/consts/types");
 const body = `${fileHeader}
 import { useState, useEffect, memo } from "react"
-export interface \${1}Props {
+// export interface \${1}Props {
   // value: any
-}
-export const \${1:${fileName}}:React.FC<\${1}Props> = ()=> {
+// }
+export const \${1:${fileName}}:React.FC/* <\${1}Props> */ = ()=> {
   return (
     <div>
       \${1}
@@ -22,8 +23,8 @@ export default $1
 const description = "react 函数组件";
 
 module.exports = {
-  prefix: ["!react"],
-  type: ["typescript", "typescriptreact"],
+  prefix: ["!react", "!r"],
+  type: [TYPES.typescript, TYPES.typescriptreact],
   description: description || body,
   body: body.trim(),
 };
