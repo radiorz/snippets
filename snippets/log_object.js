c9cc1c7c5521dd4bd3519e88e7ca2943565c8e08
tikkhun <s565338107@163.com>
2025-06-17 17:55:22 +0800

    chore: update.

--- snippets/log_object.js
+++ snippets/log_object.js
@@ -2,7 +2,7 @@ const body = `console.log(\`\$1\`,JSON.stringify(\$1,null,2))`;
 
 module.exports = {
   key: "console_log_json",
-  prefix: ["ljson"],
+  prefix: ["csljson"],
   type: ["javascript", "typescript", "javascriptreact", "typescriptreact"],
   description: "console.log(*)",
   body,
