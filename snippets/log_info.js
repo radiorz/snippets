508a21202c25f747e999e9cd54f823b4540e9b4d
tikkhun <s565338107@163.com>
2025-06-17 17:59:46 +0800

    chore: log.

--- snippets/log_info.js
+++ snippets/log_info.js
@@ -1,10 +1,9 @@
 const { JS_LIKE } = require("../src/common/consts/types");
+const body = `console.log(\`\$1\`,\$1, "\${TM_FILENAME}",\${TM_LINE_INDEX})`;
 
-const body = `console.log(\`\$1\`,\$1)`;
 module.exports = {
   key: "console_log",
-  prefix: ["csl", "logg"],
+  prefix: ["cslinfo"],
   type: JS_LIKE,
-  description: body,
   body,
 };
--- snippets/log_object.js
+++ snippets/log_object.js
@@ -1,9 +1,10 @@
+const { JS_LIKE } = require("../src/common/consts/types");
 const body = `console.log(\`\$1\`,JSON.stringify(\$1,null,2))`;
 
 module.exports = {
   key: "console_log_json",
   prefix: ["csljson"],
-  type: ["javascript", "typescript", "javascriptreact", "typescriptreact"],
-  description: "console.log(*)",
+  type: JS_LIKE,
+  description: description || body,
   body,
 };
